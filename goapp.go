e07f644b05ee2b41ee7d84e36e0bdf2d833aa453
Cesar Vega <caveligh@users.noreply.github.com>
2016-09-05 19:50:09 -0500

    Update goapp.go

--- goapp.go
+++ goapp.go
@@ -3,9 +3,11 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+	"net/http"
+	//"log"
 )
 
-// estructura de las páginas
+// Page sirve para la estructura de las páginas
 type Page struct {
 	Title string
 	Body  []byte
@@ -13,20 +15,30 @@ type Page struct {
 
 func (p *Page) save() error {
 	filename := "./data/" + p.Title + ".txt"
-	err := ioutil.WriteFile(filename, p.Body, 0600) //lee y escribe el dueño de la app
+	err := ioutil.WriteFile(filename, p.Body, 0600)
 	return err
 }
 
-func loadPage(title string) *Page {
+func loadPage(title string) (*Page, error) {
 	filename := "./data/" + title + ".txt"
-	body, _ := ioutil.ReadFile(filename)
+	body, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return nil, err
+	}
 	page := &Page{Title: title, Body: body}
-	return page
+	return page, err
+}
+
+func viewHandler(w http.ResponseWriter, r *http.Request) {
+	title := r.URL.Path[len("/view/"):]
+	p, err := loadPage(title)
+	if err != nil {
+		fmt.Fprintf(w, "<h1>%s</h1>", err)
+	}
+	fmt.Fprintf(w, "<h1>%s</h1><div>%s</div>", p.Title, p.Body)
 }
 
 func main() {
-	//page := &Page{Title: "primer", Body: []byte("Primer página")}
-	//page.save()
-	page := loadPage("primer")
-	fmt.Println(page.Title, string(page.Body))
+	http.HandleFunc("/view/", viewHandler)
+	http.ListenAndServe(":8080", nil)
 }
